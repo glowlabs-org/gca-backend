d68d04fd16fa8c3a429105e6a9746dd8de6015fc
DavidVorick <david.vorick@gmail.com>
2023-10-21 14:52:58 -0400

    repair all tests

--- report_listener_udp_helpers_test.go
+++ report_listener_udp_helpers_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/binary"
 	"fmt"
 	"net"
 )
@@ -9,15 +8,12 @@ import (
 // generateTestReport creates a mock report for testing purposes.
 // The report includes a signature based on the provided private key.
 func generateTestReport(shortID uint32, timeslot uint32, privKey PrivateKey) []byte {
-	data := make([]byte, 80)
-	binary.BigEndian.PutUint32(data[0:4], shortID)
-	binary.BigEndian.PutUint32(data[4:8], timeslot)
-	// PowerOutput can remain as zero since they don't impact the behavior we're testing.
-
-	// Sign the data using the private key and insert the signature into the report
-	signature := Sign(data[:16], privKey)
-	copy(data[16:], signature[:])
-	return data
+	er := EquipmentReport {
+		ShortID: shortID,
+		Timeslot: timeslot,
+	}
+	er.Signature = Sign(er.SigningBytes(), privKey)
+	return er.Serialize()
 }
 
 // sendUDPReport simulates sending a report to the server via UDP.
