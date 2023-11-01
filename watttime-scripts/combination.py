e43e2ca96a4d9ac138046d37ea05f8ddfed3fe11
DavidVorick <david.vorick@gmail.com>
2023-11-01 13:33:46 -0400

    keep working on script refinement

--- watttime-scripts/combination.py
+++ watttime-scripts/combination.py
@@ -1,5 +1,6 @@
 import requests
 import json
+from requests.auth import HTTPBasicAuth
 
 def prompt_for_coordinates():
     """
@@ -24,7 +25,6 @@ def fetch_nasa_data(latitude, longitude):
     Returns:
         data (dict): The JSON response from the NASA API parsed into a dictionary.
     """
-    # Define the API endpoint and parameters
     url = "https://power.larc.nasa.gov/api/temporal/daily/point"
     params = {
         "parameters": "ALLSKY_SFC_SW_DWN",
@@ -35,11 +35,8 @@ def fetch_nasa_data(latitude, longitude):
         "end": "20221231",
         "format": "json"
     }
-    
-    # Perform the API request and parse the JSON response
     response = requests.get(url, params=params)
     data = json.loads(response.text)
-    
     return data
 
 def calculate_average_sunlight(data):
@@ -52,27 +49,80 @@ def calculate_average_sunlight(data):
     Returns:
         average_sunlight (float): The average annual sunlight in kW-hr/m^2/day.
     """
-    # Extract the sunlight data points
     sunlight_data = data["properties"]["parameter"]["ALLSKY_SFC_SW_DWN"].values()
-    
-    # Filter out any fill values
     filtered_data = [x for x in sunlight_data if x != -999.0]
-    
-    # Calculate the average sunlight
     average_sunlight = sum(filtered_data) / len(filtered_data) if filtered_data else 0
-    
     return average_sunlight
 
+# New function to load credentials from a file
+def load_credentials(filename):
+    """
+    Load credentials from a given file.
+
+    Parameters:
+        filename (str): The name of the file containing the credentials.
+
+    Returns:
+        str: The credentials read from the file.
+    """
+    with open(filename, 'r') as f:
+        return f.read().strip()
+
+# New function to get the WattTime API token
+def get_token(username, password):
+    """
+    Fetch the authorization token from WattTime API.
+
+    Parameters:
+        username (str): The username for the API.
+        password (str): The password for the API.
+
+    Returns:
+        str: The authorization token.
+    """
+    login_url = 'https://api2.watttime.org/v2/login'
+    response = requests.get(login_url, auth=HTTPBasicAuth(username, password))
+    return response.json()['token']
+
+# New function to fetch the balancing authority
+def get_balancing_authority(token, latitude, longitude):
+    """
+    Fetch the balancing authority based on latitude and longitude.
+
+    Parameters:
+        token (str): The authorization token for the WattTime API.
+        latitude (float): The latitude coordinate.
+        longitude (float): The longitude coordinate.
+
+    Returns:
+        str: The balancing authority.
+    """
+    region_url = 'https://api2.watttime.org/v2/ba-from-loc'
+    headers = {'Authorization': 'Bearer {}'.format(token)}
+    params = {'latitude': latitude, 'longitude': longitude}
+    response = requests.get(region_url, headers=headers, params=params)
+    return response.json()['abbrev']
+
 if __name__ == "__main__":
-    # Step 1: Prompt user for coordinates
+    # Load WattTime API credentials
+    username = load_credentials('username')
+    password = load_credentials('password')
+
+    # Get the WattTime API token
+    token = get_token(username, password)
+
+    # Prompt user for coordinates
     latitude, longitude = prompt_for_coordinates()
-    
-    # Step 2: Fetch NASA data
+
+    # Fetch NASA data
     data = fetch_nasa_data(latitude, longitude)
-    
-    # Step 3: Calculate the average annual sunlight
+  
+    # Calculate the average annual sunlight
     average_sunlight = calculate_average_sunlight(data)
-    
-    # Step 4: Display the result
+
+    # Fetch and print the balancing authority
+    balancing_authority = get_balancing_authority(token, latitude, longitude)
+
     print(f"The average annual sunlight for the coordinates {latitude}, {longitude} is {average_sunlight} kW-hr/m^2/day.")
+    print(f"The balancing authority for the coordinates {latitude}, {longitude} is {balancing_authority}.")
 
--- watttime-scripts/historical.py
+++ watttime-scripts/historical.py
@@ -34,40 +34,39 @@ def update_gitignore(folder_name):
             if folder_name not in lines:
                 f.write(folder_name + '\n')
 
-# Load username and password from files
-username = load_from_file('username')
-password = load_from_file('password')
-
-# Construct the login URL and get the token
-login_url = 'https://api2.watttime.org/v2/login'
-token = requests.get(login_url, auth=HTTPBasicAuth(username, password)).json()['token']
-
 # Get the Balancing Authority (ba) from the user
 ba = input("Please enter the Balancing Authority (e.g., FPL): ")
 
-# Construct the historical URL and the headers
-historical_url = 'https://api2.watttime.org/v2/historical'
-headers = {'Authorization': f'Bearer {token}'}
-params = {'ba': ba}
+# Check if data for the given 'ba' already exists locally
+if path.exists(ba):
+    print(f"Data for {ba} already exists locally.")
+else:
+    # Construct the login URL and get the token
+    login_url = 'https://api2.watttime.org/v2/login'
+    token = requests.get(login_url, auth=HTTPBasicAuth(username, password)).json()['token']
 
-# Make the request to get the historical data
-rsp = requests.get(historical_url, headers=headers, params=params)
+    # Construct the historical URL and the headers
+    historical_url = 'https://api2.watttime.org/v2/historical'
+    headers = {'Authorization': f'Bearer {token}'}
+    params = {'ba': ba}
 
-# Create a new directory for the 'ba' and save the zip file there
-if not os.path.exists(ba):
-    os.mkdir(ba)
-zip_path = path.join(ba, f'{ba}_historical.zip')
+    # Make the request to get the historical data
+    rsp = requests.get(historical_url, headers=headers, params=params)
 
-with open(zip_path, 'wb') as fp:
-    fp.write(rsp.content)
+    # Create a new directory for the 'ba' and save the zip file there
+    if not os.path.exists(ba):
+        os.mkdir(ba)
+    zip_path = path.join(ba, f'{ba}_historical.zip')
+    with open(zip_path, 'wb') as fp:
+        fp.write(rsp.content)
 
-# Extract the zip file in the directory
-with zipfile.ZipFile(zip_path, 'r') as zip_ref:
-    zip_ref.extractall(ba)
+    # Extract the zip file in the directory
+    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+        zip_ref.extractall(ba)
 
-# Update the .gitignore file
-update_gitignore(ba)
+    # Update the .gitignore file
+    update_gitignore(ba)
 
-# Notify the user that the data has been written and unzipped
-print(f'Wrote and unzipped historical data for {ba} to the directory: {ba}')
+    # Notify the user that the data has been written and unzipped
+    print(f'Wrote and unzipped historical data for {ba} to the directory: {ba}')
 
