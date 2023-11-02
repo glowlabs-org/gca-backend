762d3ee72f841bcbc7d3f19308181cd16d6f7731
DavidVorick <david.vorick@gmail.com>
2023-11-01 23:35:21 -0400

    fix up plot_raw.py

--- watttime-scripts/plot_raw.py
+++ watttime-scripts/plot_raw.py
@@ -2,6 +2,8 @@ import pandas as pd
 import geopandas as gpd
 import matplotlib.pyplot as plt
 from matplotlib.colors import LinearSegmentedColormap
+from matplotlib.cm import ScalarMappable
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 # Load the CSV file into a DataFrame
 df = pd.read_csv('carbon_credits_sweep.csv')
@@ -17,35 +19,55 @@ gdf = gpd.GeoDataFrame(
 # Load GeoJSON map
 us_states = gpd.read_file('us_state_outlines.json')
 
-# Plotting
-fig, ax = plt.subplots(1, figsize=(15, 25), dpi=300)
-
 # Define continental US bounding box
 continental_bbox = {'west': -125, 'east': -66.5, 'south': 24.4, 'north': 49.3843}
 
-# Clip the states' data to the bounding box of continental US
-us_states_clipped = us_states.cx[continental_bbox['west']:continental_bbox['east'], 
-                                 continental_bbox['south']:continental_bbox['north']]
-
-# Plotting the clipped US states with a thinner border line
-us_states_clipped.boundary.plot(ax=ax, linewidth=0.25, color="black")
+# Clip the points to the bounding box of continental US
+gdf_clipped = gdf.cx[continental_bbox['west']:continental_bbox['east'], 
+                     continental_bbox['south']:continental_bbox['north']]
 
 # Define the colors for the custom colormap (blue -> green -> yellow -> orange -> red)
 colors = ['blue', 'green', 'yellow', 'orange', 'red']
-# Create a colormap object based on the colors
 cmap_name = 'custom_colormap'
-n_bins = 300  # Increase this number to have more fine transitions between colors
+n_bins = 50  # Increase this number to have more fine transitions between colors
 custom_colormap = LinearSegmentedColormap.from_list(cmap_name, colors, N=n_bins)
 
-# Plotting the real data points
-gdf.plot(column='Carbon Credits per Year per KW', 
+# Calculate the range for 'Carbon Credits per Year per KW' within the bounded area
+credit_values_within_bounds = gdf_clipped['Carbon Credits per Year per KW']
+vmin = credit_values_within_bounds.min()
+vmax = credit_values_within_bounds.max()
+
+# Create a ScalarMappable to be used for legend
+sm = ScalarMappable(cmap=custom_colormap, norm=plt.Normalize(vmin=vmin, vmax=vmax))
+sm._A = []  # Dummy array for the ScalarMappable. Required for the next step.
+
+# Plotting
+fig, ax = plt.subplots(1, figsize=(15, 25), dpi=300)
+
+# Plotting the clipped US states with a thinner border line
+us_states.cx[continental_bbox['west']:continental_bbox['east'], 
+             continental_bbox['south']:continental_bbox['north']].boundary.plot(ax=ax, linewidth=0.25, color="black")
+
+# Plotting the real data points within the bounding box
+gdf_clipped.plot(column='Carbon Credits per Year per KW', 
          cmap=custom_colormap,
          ax=ax, 
-         legend=True, 
-         legend_kwds={'label': "Carbon Credits per Year per KW"},
          marker='o',  # Circle markers
-         markersize=5)  # Size of the markers
-         
+         markersize=10)  # Size of the markers
+
+# Add color bar
+# Create an axes on the right side of ax. The width of cax will be 5%
+# of ax and the height will be matched to the bounding box of the continental US.
+divider = make_axes_locatable(ax)
+cax = divider.append_axes("right", size="5%", pad=0.1)
+
+# Create a colorbar in the axes cax with the specified tick locations
+cbar = plt.colorbar(sm, cax=cax)
+
+cbar.set_label('Carbon Credits per Year per KW')
+
+# Set the aspect of the color bar
+cbar.ax.set_aspect(20)
+
 # Show/save the plot
 plt.savefig('heatmap_raw.png', bbox_inches='tight', dpi=200)
-
