b65de879106ecc16251ad3149735656f211060b8
DavidVorick <david.vorick@gmail.com>
2023-11-25 12:16:00 -0500

    add all the missing binary code

--- glow/timeslot.go
+++ glow/timeslot.go
@@ -8,14 +8,16 @@ import (
 )
 
 const (
-	GenesisTime = 1697414400
+	// GenesisTime determines what counts as 'week 0' for the Glow
+	// protocol. It has been set to Sunday, November 19th, 2023 at 0:00:00
+	// UTC.
+	GenesisTime = 1700352000
 )
 
 // Returns the current time of the protocol, as measured in 5 minute increments
 // from genesis. This function implies a genesis time.
 func CurrentTimeslot() uint32 {
-	// TODO: Update for real network launch.
-	genesisTime := int64(GenesisTime) // Monday Oct 16, 0:00:00 UTC
+	genesisTime := int64(GenesisTime)
 	now := time.Now().Unix()
 	if now < genesisTime {
 		panic("system clock appears to be incorrect")
