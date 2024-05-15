3476cde2adcfa8ffdaae1210a66558f823e954cf
mikew686 <mike@glowlabs.org>
2024-05-15 11:02:00 -0700

    a few more tests courtesy of chatgpt

--- client/ct_settings_test.go
+++ client/ct_settings_test.go
@@ -148,3 +148,133 @@ func TestCISettingsMissingValue(t *testing.T) {
 		t.Errorf("client read the missing value ct settings file")
 	}
 }
+
+func TestCISettingsFileNotFound(t *testing.T) {
+	name := t.Name()
+	gcas, _, gcaPubkey, gcaPrivKey, err := server.SetupTestEnvironment(name + "_server1")
+	if err != nil {
+		t.Errorf("unable to set up the test environment for a server: %v", err)
+	}
+	defer gcas.Close()
+	clientDir := glow.GenerateTestDir(name + "_client1")
+	err = SetupTestEnvironment(clientDir, gcaPubkey, gcaPrivKey, []*server.GCAServer{gcas})
+	if err != nil {
+		t.Errorf("unable to set up the client test environment: %v", err)
+	}
+	// Ensure the CT settings file does not exist
+	ctPath := path.Join(clientDir, CTSettingsFile)
+	os.Remove(ctPath)
+
+	// start the client
+	client, err := NewClient(clientDir)
+	if err != nil {
+		t.Errorf("unable to create client: %v", err)
+	}
+	defer client.Close()
+
+	if client.energyMultiplier != EnergyMultiplierDefault || client.energyDivider != EnergyDividerDefault {
+		t.Errorf("default settings not applied: got multiplier %v and divider %v, expected %v and %v",
+			client.energyMultiplier, client.energyDivider, EnergyMultiplierDefault, EnergyDividerDefault)
+	}
+}
+
+func TestCISettingsEmptyFile(t *testing.T) {
+	name := t.Name()
+	gcas, _, gcaPubkey, gcaPrivKey, err := server.SetupTestEnvironment(name + "_server1")
+	if err != nil {
+		t.Errorf("unable to set up the test environment for a server: %v", err)
+	}
+	defer gcas.Close()
+	clientDir := glow.GenerateTestDir(name + "_client1")
+	err = SetupTestEnvironment(clientDir, gcaPubkey, gcaPrivKey, []*server.GCAServer{gcas})
+	if err != nil {
+		t.Errorf("unable to set up the client test environment: %v", err)
+	}
+	// create an empty ct settings file
+	ctPath := path.Join(clientDir, CTSettingsFile)
+	ctf, err := os.Create(ctPath)
+	if err != nil {
+		t.Errorf("could not open ct settings file: %v", err)
+	}
+	ctf.Close()
+
+	// try to start the client
+	client, err := NewClient(clientDir)
+	if err == nil {
+		client.Close()
+		t.Errorf("client read the empty ct settings file")
+	}
+}
+
+func TestCISettingsNonNumericValues(t *testing.T) {
+	name := t.Name()
+	gcas, _, gcaPubkey, gcaPrivKey, err := server.SetupTestEnvironment(name + "_server1")
+	if err != nil {
+		t.Errorf("unable to set up the test environment for a server: %v", err)
+	}
+	defer gcas.Close()
+	clientDir := glow.GenerateTestDir(name + "_client1")
+	err = SetupTestEnvironment(clientDir, gcaPubkey, gcaPrivKey, []*server.GCAServer{gcas})
+	if err != nil {
+		t.Errorf("unable to set up the client test environment: %v", err)
+	}
+	// create a ct settings file with non-numeric values
+	ctPath := path.Join(clientDir, CTSettingsFile)
+	ctf, err := os.Create(ctPath)
+	if err != nil {
+		t.Errorf("could not open ct settings file: %v", err)
+	}
+	ctContent := "not-a-number\nanother-non-number\n"
+	_, err = ctf.WriteString(ctContent)
+	if err != nil {
+		ctf.Close()
+		t.Errorf("unable to write ct settings file: %v", err)
+	}
+	ctf.Close()
+
+	// try to start the client
+	client, err := NewClient(clientDir)
+	if err == nil {
+		client.Close()
+		t.Errorf("client read the non-numeric ct settings file")
+	}
+}
+
+func TestCISettingsLargeValues(t *testing.T) {
+	name := t.Name()
+	gcas, _, gcaPubkey, gcaPrivKey, err := server.SetupTestEnvironment(name + "_server1")
+	if err != nil {
+		t.Errorf("unable to set up the test environment for a server: %v", err)
+	}
+	defer gcas.Close()
+	clientDir := glow.GenerateTestDir(name + "_client1")
+	err = SetupTestEnvironment(clientDir, gcaPubkey, gcaPrivKey, []*server.GCAServer{gcas})
+	if err != nil {
+		t.Errorf("unable to set up the client test environment: %v", err)
+	}
+	// create a ct settings file with large numeric values
+	ctPath := path.Join(clientDir, CTSettingsFile)
+	ctf, err := os.Create(ctPath)
+	if err != nil {
+		t.Errorf("could not open ct settings file: %v", err)
+	}
+	ctContent := "1e10\n1e10\n"
+	_, err = ctf.WriteString(ctContent)
+	if err != nil {
+		ctf.Close()
+		t.Errorf("unable to write ct settings file: %v", err)
+	}
+	ctf.Close()
+
+	// start the client
+	client, err := NewClient(clientDir)
+	if err != nil {
+		t.Errorf("unable to create client: %v", err)
+	}
+	defer client.Close()
+
+	if client.energyMultiplier != 1e10 || client.energyDivider != 1e10 {
+		t.Errorf("unexpected large values: got multiplier %v and divider %v, expected %v and %v",
+			client.energyMultiplier, client.energyDivider, 1e10, 1e10)
+	}
+}
