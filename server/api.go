584e6356a353076c3b93c92095dfa22ed8a8e516
mikew686 <mike@glowlabs.org>
2024-05-14 17:34:07 -0700

    remove unneeded internal api

--- server/api.go
+++ server/api.go
@@ -18,7 +18,6 @@ func (gcas *GCAServer) launchAPI() {
 	gcas.mux.HandleFunc("/api/v1/recent-reports", gcas.RecentReportsHandler)
 	gcas.mux.HandleFunc("/api/v1/geo-stats", gcas.GeoStatsHandler)
 	// Internal APIs which will not be accessible except under bench testing mode
-	gcas.mux.HandleFunc("/api/int/wt-historical", gcas.InternalWattTimeHistoricalHandler)
 	gcas.mux.HandleFunc("/api/int/wt-signal-index", gcas.InternalWattTimeSignalIndexHandler)
 
 	// Create a listener. In prod it's a specfic port, during testing it's
